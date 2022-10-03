f8be75ced8e93834ad4169ec46b5d1f9488d691e
Dangiras <dan.hausmans@gmail.com>
2022-10-03 11:54:13 +0200

    finished the form + quest 2

---
+++ front/red-thread/src/components/SignUp.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+class SignUp extends React.Component {
+  constructor(toto) {
+    super(toto);
+    this.state = {
+      email: "",
+    };
+  }
+
+  updateEmailField = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  render() {
+    const { email } = this.state;
+    const { updateEmailField } = this;
+
+    return (
+      <>
+        <h1>{email}</h1>
+        <form>
+          <input type="email" name="email" onChange={updateEmailField} />
+        </form>
+      </>
+    );
+  }
+}
+
+export default SignUp;
